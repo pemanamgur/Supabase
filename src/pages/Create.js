fa99626ce2c0b379c04caeb9cf9a059df7342713
Pema@777 <pemanamgurtamang7@gmail.com>
2024-01-17 09:19:08 +0545

    create

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,28 +1,60 @@
-import { useState } from "react"
+import { useState } from "react";
+import supabase from "../config/supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [created_at, setCreated_At] = useState('');
+  const [formError, setFormError] = useState(null);
 
-    const [name , setName] = useState('');
-    const [created_at , setCreated_At] = useState('');
-    const handleSubmit =()=>{
-      console.log('hello');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !created_at) {
+      setFormError('Please fill in all the fields correctly.');
+      return;
     }
+
+    const { data, error } = await supabase
+      .from('Supa')
+      .insert([{ name, created_at }]);
+
+      if (error) {
+        console.log(error)
+        setFormError('Please fill in all the fields correctly.')
+      }
+      if (data) {
+        console.log(data)
+        setFormError(null)
+        navigate('/')
+      }
+    }
+
   return (
     <div className="page create">
-       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name :</label>
-        <input 
-        type="text" 
-        id="name"
-        />    
-        <label htmlFor="created_at">Created_At :</label>
-        <textarea 
-         id="created_at"
-         
-         ></textarea>
-       </form>
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-export default Create
+        <label htmlFor="created_at">Created_At:</label>
+        <input
+          type="date"
+          id="created_at"
+          value={created_at}
+          onChange={(e) => setCreated_At(e.target.value)}
+        />
+
+        <button>Create Smoothie Recipe</button>
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Create;
